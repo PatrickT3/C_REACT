31e968441e2b1e7c2444e4084836ab0fb8055145
Patrick Teixeira <tp1002251@gmail.com>
2023-09-19 15:47:14 -0300

    organizando a navbar do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Link } from "react-router-dom";
+import logo from './assets/react.svg';
 
 function App() {
- 
 
   return (   
-    <nav>
-      <ul>
+    <nav className='naveprinc'>
+      <img src={logo} alt="Logo" className='img' />
+      <ul className='nave'>
         <li>
           <Link to="/">Home</Link>
         </li>
