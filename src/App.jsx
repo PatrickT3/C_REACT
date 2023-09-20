a88f3b606f9cc360aa92be31ae03b44f53d30c32
Patrick Teixeira <tp1002251@gmail.com>
2023-09-20 12:01:28 -0300

    estilizando o nav ativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import './App.css'
 import { Link } from "react-router-dom";
 import logo from './assets/react.svg';
+import { useState } from 'react';
 
 function App() {
-
-  return (   
-    <nav className='naveprinc'>
-      <img src={logo} alt="Logo" className='img' />
-      <ul className='nave'>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/AboutMe">About Me</Link>
-        </li>
-        <li>
-          <Link to="/PExperience">PExperience</Link>
-        </li>
-        <li>
-          <Link to="/Skills">Skills</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+  
+    const [ativo, setAtivo] = useState('');
+  
+    const atualizarAtivo = (novoValor) => {
+      setAtivo(novoValor);
+    }
+  
+    return (
+      <nav className='naveprinc'>
+        <img src={logo} alt="Logo" className='img' />
+        <ul className='nave'>
+          <li>
+            <Link 
+              to="/" 
+              onClick={() => atualizarAtivo('Home')} 
+              className={ativo === 'Home' ? 'linksAtivo' : 'links'} 
+            >Home</Link>
+          </li>
+          <li>
+            <Link 
+              to="/AboutMe" 
+              onClick={() => atualizarAtivo('About Me')} 
+              className={ativo === 'About Me' ? 'linksAtivo' : 'links'} 
+            >About Me</Link>
+          </li>
+          <li>
+            <Link 
+              to="/PExperience" 
+              onClick={() => atualizarAtivo('PExperience')} 
+              className={ativo === 'PExperience' ? 'linksAtivo' : 'links'} 
+            >Experience</Link>
+          </li>
+          <li>
+            <Link 
+              to="/Skills" 
+              onClick={() => atualizarAtivo('Skills')} 
+              className={ativo === 'Skills' ? 'linksAtivo' : 'links'} 
+            >Skills</Link>
+          </li>
+          <li>
+            <Link to="/contact" 
+            onClick={() => atualizarAtivo('Contact')} 
+            className={ativo === 'Contact' ? 'linksAtivo' : 'links'} 
+          >Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 
 export default App
