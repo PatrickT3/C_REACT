5ce7bb2cdb07465d1ef673edc83d2f9125abfd66
Patrick Teixeira <tp1002251@gmail.com>
2023-09-27 15:05:43 -0300

    AbouteMe page completa

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,16 @@
 import './Card.css'
+import PropTypes from 'prop-types';
 
-const Card = () => {
+const Card = ({texth2,textp}) => {
   return (
-    <div>Card</div>
+    <div className='Card'>
+      <h2>{texth2}</h2>
+      <p>{textp}</p>
+    </div>
   )
 }
-
+Card.propTypes = {
+  texth2: PropTypes.string.isRequired, // Validação da propriedade 'text'
+  textp: PropTypes.string.isRequired, // Validação da propriedade 'text'
+};
 export default Card
--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,6 +1,31 @@
+import './AboutMe.css'
+import astro from '../../img/astro.png'
+import Card from '../../components/Card/Card'
+
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className="aprinc">
+      <h1>About Me</h1>
+      <p>My introduction</p>
+      <div className='body'>
+        <div className='lee'>
+          <img src={astro} alt="Astronautilos" />
+        </div>
+        <div className='ldd'>
+          <div className='lddCard'>
+            <Card texth2="Experience" textp="+1 years"/>
+            <Card texth2="Experience" textp="+1 years"/>
+            <Card texth2="Experience" textp="+1 years"/>
+          </div>
+          <p className='area-de-texto'>
+            Se você é criativo, tem um olho afiado para o design e é 
+            apaixonado por criar experiências de usuário excepcionais,
+            este é o cargo para você. O desenvolvedor front-end desempenhará 
+          </p>
+          <button className='btn-link'>Download CV</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
