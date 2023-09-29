b1eb63204218355003cf25e91accfc6438da44ac
Patrick Teixeira <tp1002251@gmail.com>
2023-09-28 22:49:10 -0300

    mudando as cores do icons do componete card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,15 @@
 import './Card.css'
 import PropTypes from 'prop-types';
 import {AiFillStar} from 'react-icons/ai';
-import {AiFillTrophy} from 'react-icons/ai';
+import {FcClock} from 'react-icons/fc';
 import {AiOutlineCheckCircle} from 'react-icons/ai';
 
 const Card = ({texth2,textp,num}) => {
   return (
     <div className='Card'>
-      {num === 1 && <AiFillStar></AiFillStar> }
-      {num === 2 && <AiFillTrophy></AiFillTrophy> }
-      {num === 3 && <AiOutlineCheckCircle></AiOutlineCheckCircle> }
+      {num === 1 && <AiFillStar id='tro'></AiFillStar> }
+      {num === 2 && <FcClock></FcClock> }
+      {num === 3 && <AiOutlineCheckCircle id='ok'></AiOutlineCheckCircle> }
       <h2>{texth2}</h2>
       <p>{textp}</p>
     </div>
