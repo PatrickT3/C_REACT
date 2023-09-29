c95d6b1db73db55639439a185572d5c6200da5e8
Patrick Teixeira <tp1002251@gmail.com>
2023-09-29 16:35:50 -0300

    Finalizacao do PEx

--- src/pages/PExperience/PEx.jsx
+++ src/pages/PExperience/PEx.jsx
@@ -1,7 +1,8 @@
 import './PEx.css'
 import {FaGraduationCap} from 'react-icons/fa';
 import {FcCalendar} from 'react-icons/fc';
-import {LuBaggageClaim} from 'react-icons/lu';
+import {PiSuitcaseSimpleBold} from 'react-icons/pi';
+import ufo from '../../img/Ufopa.webp';
 
 const PEx = () => {
   return (
@@ -12,9 +13,30 @@ const PEx = () => {
       <div className='containerflex'>
         <div className='left'>
           <h3><FaGraduationCap></FaGraduationCap> Education</h3>
+          <div className='bor'>
+            <h4 id='ufopa'><img src={ufo} alt="Ufopa" />UFOPA</h4>
+            <p>Sistema de Informação <br/>
+            2018 - Present</p>
+          </div>
+        </div>
+        <div className='meio'>
         </div>
         <div className='right'>
-          <h3><LuBaggageClaim></LuBaggageClaim> Experience</h3>    
+          <h3><PiSuitcaseSimpleBold></PiSuitcaseSimpleBold> Experience</h3>    
+          <div className='bor2'>
+            <h3 id='PO'>Prefeitura de Oriximiná, Estágio</h3>
+            <p>
+              <span>CPD: Centro de Processamento de Dados</span><br/>
+              From April 2021 To September 2021
+            </p>
+          </div>
+          <div className='bor3'>
+            <h3 id='PO2'>Compasso UOL, Estágio</h3>
+            <p>
+              <span>developer Front-end & Cloud with AWS</span><br/>
+              From December 2022 To May 2023
+            </p>
+          </div>
         </div>
       </div>
     </div>
