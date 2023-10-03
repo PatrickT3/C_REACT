07522298f3b07853bcba7ccf7e09fab39042251c
Patrick Teixeira <tp1002251@gmail.com>
2023-10-03 16:05:55 -0300

    Colocando os link para o perfil correspondente

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,13 +2,15 @@ import './Footer.css'
 import { TfiLinkedin } from 'react-icons/tfi'
 import {FaInstagramSquare} from 'react-icons/fa'
 import {BsGithub} from 'react-icons/bs'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className='Footer'>
-        <FaInstagramSquare className='inst'/>
-        <TfiLinkedin className='linkd'/>
-        <BsGithub className='git'/>
+      <Link to="https://www.instagram.com/steixeirapatrick/"><FaInstagramSquare className='inst'/></Link>
+      <Link to="https://www.linkedin.com/in/patrick-teixeira-baa8b824a"><TfiLinkedin className='linkd'/></Link>
+      <Link to="https://github.com/PatrickT3"><BsGithub className='git'/></Link>
+        
     </div>
   )
 }
